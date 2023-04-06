9788dc19abeed34ae94424abd8833646c0a9ee24
Igor Klein <igorklein1988@gmail.com>
2023-04-06 21:09:51 +0200

    getFolderSize

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String folderPath = "/users/igorklein/IdeaProjects";
+        File file = new File(folderPath);
+        System.out.println(file.length());
+
+        System.out.println(System.getProperties().get("user.dir")); // ! BiggestFolderFinder
+
+        System.out.println("getFolderSize = " + getFolderSize(file)); // ! 5944955 это байты
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()){
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
