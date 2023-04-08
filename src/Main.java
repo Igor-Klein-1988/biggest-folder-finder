03b9a9f68473a7aec51dd0b56f54945de4e31af6
Igor Klein <igorklein1988@gmail.com>
2023-04-08 23:30:23 +0200

    add ForkJoinPool

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 import java.io.File;
-import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "/users/igorklein/IdeaProjects";
         File file = new File(folderPath);
-        System.out.println(file.length());
+//        System.out.println(file.length());
 
-        System.out.println(System.getProperties().get("user.dir")); // ! BiggestFolderFinder
+//        System.out.println(System.getProperties().get("user.dir")); // ! BiggestFolderFinder
+//
+//        System.out.println("getFolderSize = " + getFolderSize(file)); // ! 5_944_955 это байты
 
-        System.out.println("getFolderSize = " + getFolderSize(file)); // ! 5944955 это байты
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = (long) pool.invoke(calculator);
+        System.out.println("size long" + size);
     }
 
     public static long getFolderSize(File folder) {
-        if (folder.isFile()){
+        if (folder.isFile()) {
             return folder.length();
         }
 
